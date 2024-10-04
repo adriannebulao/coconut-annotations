5fc49a58b692dc063fa09ac6b4d8502e23db0e4f
adriannebulao <amcbulao@gmail.com>
2024-10-04 12:31:29 +0800

    feat: add batch/shell scripts for setting env variables

---
+++ set_env.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+export LABEL_STUDIO_LOCAL_FILES_SERVING_ENABLED=true
+export LABEL_STUDIO_LOCAL_FILES_DOCUMENT_ROOT=/home/adrianne/Desktop/code/coconut-annotations/storage
+python3 update_paths.py
